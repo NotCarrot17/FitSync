b03405daa4b6e8dc246a864ba7fa1f59a68e9820
freshavocadoe <boomitsaca@gmail.com>
2024-12-07 04:30:58 +0800

    update2

--- src/main/java/com/example/fitsync/controller/WorkoutPageController.java
+++ src/main/java/com/example/fitsync/controller/WorkoutPageController.java
@@ -1,4 +1,12 @@
 package com.example.fitsync.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class WorkoutPageController {
+
+    @Override public void start(Start stage)
 }
