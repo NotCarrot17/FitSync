be28f225da31ec6a7f57248cb0a97b411a377699
ilallogical <nabilaazlan2001@gmail.com>
2024-12-09 23:40:15 +0800

    Just Adding FXML and JavaFx for FitnessTracker Page

---
+++ src/main/java/com/example/fitsync/controller/FitnessTrackerController.java
@@ -0,0 +1,15 @@
+package com.example.fitsync.controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FitnessTrackerController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
