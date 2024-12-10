40419a6b2a2f35d4f0fe516f7d908dd7d0f238f9
ilallogical <nabilaazlan2001@gmail.com>
2024-12-10 20:31:42 +0800

    I lost work cuz I didnt commit and Im going Insane

--- src/main/java/com/example/fitsync/controller/FitnessTrackerController.java
+++ src/main/java/com/example/fitsync/controller/FitnessTrackerController.java
@@ -1,15 +1,15 @@
 package com.example.fitsync.controller;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class FitnessTrackerController extends Application {
+import com.example.fitsync.dao.UserDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class FitnessTrackerController{
 
-    @Override
-    public void start(Stage primaryStage) {
-        
-    }
 }
