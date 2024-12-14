7a97dace3c72435a4680638cfb8be8dae1e772ac
freshavocadoe <boomitsaca@gmail.com>
2024-12-15 00:38:31 +0800

    final?

--- src/main/java/com/example/fitsync/controller/WorkoutPageController.java
+++ src/main/java/com/example/fitsync/controller/WorkoutPageController.java
@@ -7,8 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
